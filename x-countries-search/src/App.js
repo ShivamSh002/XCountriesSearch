d9e7aa8b7d7ab11f0805fd9c6313402ffc87c95c
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-29 14:40:11 +0530

    final commit

--- x-countries-search/src/App.js
+++ x-countries-search/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  const getData = async () => {
+    try {
+      let response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchedCountry.toLowerCase())
+  );
+
+  const cardWrapper = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="input">
+        <input
+          value={searchedCountry}
+          onChange={(e) => setSearchedCountry(e.target.value)}
+          placeholder="Search for countries..."
+        />
+      </div>{" "}
+      <div style={cardWrapper}>
+        {" "}
+        {filteredCountries.map((item) => (
+          <div key={item.cca3} className="countryCard">
+            <img
+              src={item.flags.png}
+              alt={`Flag of ${item.name.common}`}
+              style={{ width: "100px", height: "100px" }}
+              width="100"
+              height="100"
+            />
+            <h2> {item.name.common} </h2>{" "}
+          </div>
+        ))}{" "}
+      </div>{" "}
     </div>
   );
-}
+};
 
 export default App;
